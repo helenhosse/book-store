f308cc5b19d1efb53d48d8723aa340ce5900c5cb
johndean3326 <113262365+johndean3326@users.noreply.github.com>
2023-03-27 21:30:35 -0600

    installs
    install mongoose, nodemon

--- server/models/index.js
+++ server/models/index.js
@@ -2,4 +2,7 @@ const User = require('./User');
 const Products = require('./Products');
 const Category = require('./Category');
 
+
+
+
 module.exports = { User, Products, Category };
--- server/server.js
+++ server/server.js
@@ -2,9 +2,15 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 require("dotenv").config();
-const db = require('./config/connection');
-
+const connectDB = require('./config/connection');
+const mongoose = require('mongoose';)
 const PORT = process.env.PORT || 3001;
 
+connectDB();
+
+app.use(logger);
+app.use(credentials);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // need to add apolloserver, middleware and static assets once made and graphQL
