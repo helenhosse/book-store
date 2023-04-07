370c9afa6d876d26d256b2f7d1a7593f8b6429e2
JacobYaws <Jacob.Yaws@du.edu>
2023-04-06 20:23:34 -0600

    Updated connection.js. Commented out all of the existing code in case it needs to be used later.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,23 +1,38 @@
+// require('dotenv').config();
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect(process.env.DATABASE_URI,{
+//             useUnifiedTopology: true,
+//             useNewUrlParser: true
+//         });
+//     } catch (err) {
+//         console.error(err);
+//     }
+// }
+// // add the connection
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://71.196.133.50/32",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }
+// );
+
+// module.exports = connectDB
+
 require('dotenv').config();
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DATABASE_URI,{
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-        });
-    } catch (err) {
-        console.error(err);
-    }
-}
-// add the connection
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://71.196.133.50/32",
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/bookstore",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
-);
+)
+  .then(() => console.log("MongoDB has been connected"))
+  .catch((err) => console.log(err));
 
-module.exports = connectDB
+module.exports = mongoose.connection
